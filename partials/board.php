d4ff9489c8c545a9f37022365599b85c9182306a
Matt D <mattsd1410@gmail.com>
2018-10-17 21:51:33 +1100

    Clean up the board template

--- partials/board.php
+++ partials/board.php
@@ -10,35 +10,34 @@ cantt_partial('layout/start.php', array(
 
 
 <section class="board">
-	<header>
-		<ul>
+	<header class="board__header">
+		<ul class="board__header-group board__header-group--major">
 			<li>
-				<p>October</p>
-				<ul>
+				<p class="board__header-group--major__title">October</p>
+				<ul class="board__header-group board__header-group--minor">
 					<li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li><li>18</li><li>19</li><li>20</li><li>21</li><li>22</li><li>23</li><li>24</li><li>25</li><li>26</li><li>27</li><li>28</li><li>29</li><li>30</li><li>31</li>
 				</ul>
 			</li>
 			<li>
-				<p>November</p>
-				<ul>
+				<p class="board__header-group--major__title">November</p>
+				<ul class="board__header-group board__header-group--minor">
 					<li>1</li><li>2</li><li>3</li><li>4</li><li>5</li><li>6</li><li>7</li><li>8</li><li>9</li><li>10</li><li>11</li><li>12</li><li>13</li><li>14</li><li>15</li><li>16</li><li>17</li><li>18</li><li>19</li><li>20</li><li>21</li><li>22</li><li>23</li><li>24</li><li>25</li><li>26</li><li>27</li><li>28</li><li>29</li><li>30</li><li>31</li>
 				</ul>
 			</li>
 		</ul>
 	</header>
 
-	<div>
-		<ul>
-			<li>
-				<label>Figure this out</label>
-				<div style="left:120px;width:60px;"></div>
-			</li>
-			<li>
-				<label>This one's longer and hopefully linebreaks</label>
-				<div style="left:240px;width:60px;"></div>
-			</li>
-		</ul>
-	</div>
+	<ul class="board__tasks">
+		<li class="board-task">
+			<label class="board-task__label">Figure this out</label>
+			<div class="board-task__item board-task__item--inactive" style="margin-left:60px;width:90px;"></div>
+			<div class="board-task__item board-task__item--active" style="margin-left:0px;width:60px;"></div>
+		</li>
+		<li class="board-task">
+			<label class="board-task__label">This one's longer and hopefully linebreaks</label>
+			<div class="board-task__item board-task__item--active" style="margin-left:240px;width:60px;"></div>
+		</li>
+	</ul>
 </section>
 
 <?php
